401d6221feb60e085af72145347358d603f97c14
Jong-won Choi <jong-won.choi@defstruct.com>
2011-11-10 09:34:02 +1100

    Commit before renaming tbw -> core

--- src/tbw/tbw.clj
+++ src/tbw/tbw.clj
@@ -12,19 +12,38 @@
 ;;;; Code:
 
 (ns tbw.tbw
-  (:import [java.io :only []]))
+  (:use ring.adapter.jetty)
+  ;;(:import [java.io :only []]))
+  )
+
+;; Global mapping used in top level dispatcher
+(def tbw-sites (ref {}))
+
+(defn- top-level-dispatcher [request]
+  )
 
 (defrecord TemplateBasedWebSite [script->html-template site-prefix home-page-url site-dispatchers common-template-var-fn])
 
-(def tbw-sites {})
-
-(defn make-tbw-site [& {:keys [site-prefix home-page-url site-dispatchers common-template-var-fn]
-                        :or {site-prefix "FIXME"}}]
+(defn- make-tbw-site [site-name & {:keys [site-prefix home-page-url site-dispatchers common-template-var-fn]
+                                  :or {site-prefix "FIXME"}}]
   {:pre [(identity home-page-url) (identity site-dispatchers)]}
-  (let [
-  (TemplateBasedWebSite. {} ;; script->html-template
-                         site-prefix
-                         ))
+  (let [new-site (TemplateBasedWebSite. {} ;; script->html-template
+                                        site-prefix
+                                         home-page-url
+                                         site-dispatchers
+                                         common-template-var-fn)]
+
+
+    new-site))
+
+(defn- update-tbw-sites! [site-name site-obj]
+  (let [existing-def (get @tbw-sites site-name)]
+    (when existing-def
+      ;; FIXME: warn!
+      )
+    (dosync
+     (alter tbw-sites assoc site-name site-obj))))
+
 
 (defmacro def-tbw [site-name [prefix]
                    & {:keys [site-home site-dispatchers html-page-defs
@@ -34,8 +53,8 @@
      (defn ~site-name []
        ;; return html page based on "site-name + script-name"
        )
-     ;; update global record table
-     ;; E.g. remove existing one, put new one
+     (update-tbw-sites! site-name (make-tbw-site :site-prefix prefix
+                                                 :site-home site-home
   ))
 
 (def-tbw ex-website [] ;; or ("prefix"). For example ("subdomain")
