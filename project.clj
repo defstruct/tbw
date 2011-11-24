e7c32f0f169873af7f074b8fec1205cec21c6681
Jong-won Choi <jong-won.choi@defstruct.com>
2011-11-24 23:26:27 +1100

    0.1.1 RC2 version bump

--- project.clj
+++ project.clj
@@ -35,7 +35,7 @@
 ;;
 ;;;; Code:
 
-(defproject tbw "0.1.1-RC1-SNAPSHOT"
+(defproject tbw "0.1.1-RC2"
   :description "tbw - template based web"
   :url "https://github.com/defstruct/tbw"
   :dependencies [[org.clojure/clojure "1.3.0"]
